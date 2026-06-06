c5c08191428bb865e0050c63730c217c2261ae01
Saif Shalan <safensound95@gmail.com>
2026-06-06 18:02:15 +0300

    feat: implement centralized error handling and blogFinder middleware

---
+++ utils/middleware.js
@@ -0,0 +1,25 @@
+const { Blog } = require("../models");
+
+const blogFinder = async (req, res, next) => {
+  req.blog = await Blog.findByPk(req.params.id);
+
+  if (!req.blog) {
+    return res.status(404).end();
+  }
+
+  next();
+};
+
+const errorHandler = (error, req, res, next) => {
+  console.error(error.message);
+
+  if (error.name === "SequelizeDatabaseError") {
+    return res.status(400).json({ error: error.message });
+  } else if (error.name === "SequelizeValidationError") {
+    return res.status(400).json({ error: error.message });
+  }
+
+  next(error);
+};
+
+module.exports = { blogFinder, errorHandler };
